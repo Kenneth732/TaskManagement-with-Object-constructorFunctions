45e4a859b445b8c3ada17521e03073f653f9a622
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-06 10:55:08 +0300

    Recipe Constractor Func...

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+function Recipe(title, ingredients, instructions) {
+    this.title = title;
+    this.ingredients = ingredients;
+    this.instructions = instructions;
+  
+    this.displayDetails = function() {
+      console.log("Title: " + this.title);
+      console.log("Ingredients: " + this.ingredients.join(", "));
+      console.log("Instructions: " + this.instructions);
+    };
+  }
+  
+
+
